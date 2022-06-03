e9a29148a21380c78dec28c31a3cbcc9f900f38f
CrashOverride <20084833@mail.wit.ie>
2022-06-03 19:22:54 +0100

    Using react-query to manage the browser-based cache.

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,18 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react"; // useState/useEffect redundant 
 import { withRouter } from "react-router-dom";
 import MovieDetails from "../components/movieDetails";
 import PageTemplate from "../components/templateMoviePage";
 import { getMovie } from "../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner'
 
 const MovieDetailsPage = (props) => {
-  const { id } = props.match.params;
-  const [movie, setMovie] = useState(null);
+  const { id } = props.match.params
 
-  useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
-    });
-  }, [id]);
+  const { data: movie, error, isLoading, isError } = useQuery(
+    ["movie", { id: id }],
+    getMovie
+  );
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
 
   return (
     <>
